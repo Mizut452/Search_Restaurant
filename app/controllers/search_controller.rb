12e4ab7511f2e4ba74a68c0dc7dc38499ef2bc5b
Mizut452 <ssvror444@gmail.com>
2023-04-23 19:42:50 +0900

    受け取ったデータのXMLをRubyハッシュ化させる

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,20 @@
 class SearchController < ApplicationController
   API_KEY = 'de7e8ea5ccd50a30'
   def search
-    require 'rexml/document'
+    require 'nokogiri'
 
     raw_response = Faraday.get "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=de7e8ea5ccd50a30&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}"
+    #nokogiriによってXMLデータをRubyのハッシュ化させる
+    doc = Nokogiri::XML(raw_response)
+    
+    data = Hash.from_xml(doc.to_s)
 
-    @result = REXML::Document.new(raw_response.body)
+    results = []
+    data['results']['shop'].each do |d|
+      results << {
+        name: shop['name'],
+        access: shop['access']
+
+      }
   end
 end
