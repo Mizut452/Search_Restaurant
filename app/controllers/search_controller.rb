045acd137c583c8f07b30230e465ad14ce93252b
Mizut452 <ssvror444@gmail.com>
2023-04-23 13:36:22 +0900

    APIの受け取り完了

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
+  API_KEY = 'de7e8ea5ccd50a30'
   def search
-      
+    require 'rexml/document'
+
+    
+
+    raw_response = Faraday.get "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=" + API_KEY + "&large_area=Z011"
+    @result = REXML::Document.new(raw_response.body)
   end
 end
