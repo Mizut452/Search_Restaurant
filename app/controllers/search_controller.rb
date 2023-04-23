6d9b5aef922341f1b304b6c3d38cbea40eadf8d4
Mizut452 <ssvror444@gmail.com>
2023-04-23 19:50:05 +0900

    検索データを出力できるように

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,16 +5,17 @@ class SearchController < ApplicationController
 
     raw_response = Faraday.get "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=de7e8ea5ccd50a30&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}"
     #nokogiriによってXMLデータをRubyのハッシュ化させる
-    doc = Nokogiri::XML(raw_response)
+    doc = Nokogiri::XML(raw_response.body)
     
     data = Hash.from_xml(doc.to_s)
 
-    results = []
+    #ハッシュ化されたデータから表示したいデータを抽出し、配列に入れる。
+    @results = []
     data['results']['shop'].each do |d|
-      results << {
-        name: shop['name'],
-        access: shop['access']
-
+      @results << {
+        name: d['name'],
+        access: d['access']
       }
   end
+end
 end
